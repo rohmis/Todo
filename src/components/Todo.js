bd703ca4b43578bb8cf5f629cf743addcc489e17
rohmis <romishra@bestpeers.in>
2023-12-21 16:39:37 +0530

    added comments for each and every functionality

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -11,15 +11,18 @@ import {
 import axios from "axios";
 
 export default function Todo() {
+  // Initial state for Todo and form fields
   const initialData = {
     title: "",
     description: "",
   };
 
+  // States to manage Todos, form data, and editing state
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState(initialData);
   const [editTodo, setEditTodo] = useState(null);
 
+  // Fetch Todos from API on component mount
   const fetchTodos = () => {
     axios
       .get("http://127.0.0.1:8000/api/Todo/")
@@ -35,11 +38,15 @@ export default function Todo() {
     fetchTodos();
   }, []);
 
+  // Function to add a new Todo
   const handleAddTodo = () => {
+    // Send a POST request to add a new Todo
     axios
       .post("http://127.0.0.1:8000/api/Todo/", newTodo)
       .then((response) => {
+        // Update Todos state with the newly added Todo
         setTodos([...todos, response.data]);
+        // Reset form fields to initial state
         setNewTodo(initialData);
       })
       .catch((error) => {
@@ -47,19 +54,25 @@ export default function Todo() {
       });
   };
 
+  // Function to handle editing of a Todo
   const handleEdit = (id) => {
+    // Find the Todo to edit by ID and set it to editTodo state
     const todoToEdit = todos.find((todo) => todo.id === id);
     setEditTodo(todoToEdit);
   };
 
+  // Function to update an existing Todo
   const handleUpdateTodo = () => {
+    // Send a PUT request to update the Todo
     axios
       .put(`http://127.0.0.1:8000/api/Todo/${editTodo.id}/`, editTodo)
       .then((response) => {
+        // Update Todos state with the updated Todo
         const updatedData = todos.map((todo) =>
           todo.id === editTodo.id ? response.data : todo
         );
         setTodos(updatedData);
+        // Reset editTodo state after updating
         setEditTodo(null);
       })
       .catch((error) => {
@@ -67,10 +80,13 @@ export default function Todo() {
       });
   };
 
+  // Function to delete a Todo
   const handleDelete = (id) => {
+    // Send a DELETE request to remove the Todo
     axios
       .delete(`http://127.0.0.1:8000/api/Todo/${id}/`)
       .then(() => {
+        // Filter out the deleted Todo and update Todos state
         const updatedData = todos.filter((todo) => todo.id !== id);
         setTodos(updatedData);
       })
@@ -81,15 +97,14 @@ export default function Todo() {
 
   return (
     <Container fluid>
-
-       {/* Todo Form */}
-       
+      {/* Todo Form */}
       <Row className="justify-content-center align-items-center">
         <Col xs={12} md={6} className="w-40">
           <Card className="bg-dark text-white my-3 mx-auto rounded-3">
             <Card.Body className="p-3 d-flex flex-column align-items-center">
               <h2 className="fw-bold mb-4 text-uppercase">Todo</h2>
               <Form className="w-100">
+                {/* Form fields for Title and Description */}
                 <Form.Group className="mb-4">
                   <Form.Label>Title :</Form.Label>
                   <Form.Control
@@ -125,6 +140,7 @@ export default function Todo() {
                     }
                   />
                 </Form.Group>
+                {/* Add or Update Todo Button */}
                 <div className="d-grid gap-2">
                   <Button
                     variant="outline-light"
@@ -155,12 +171,14 @@ export default function Todo() {
               </tr>
             </thead>
             <tbody>
+              {/* Displaying Todos in a Table */}
               {todos.map((data) => (
                 <tr key={data.id}>
                   <td>{data.id}</td>
                   <td>{data.title}</td>
                   <td>{data.description}</td>
                   <td className="text-center">
+                    {/* Buttons for Edit and Delete */}
                     <Button
                       variant="outline-dark"
                       className="me-2"
